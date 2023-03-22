9c89176a3b1a4d999c0c0680a9cb4dd0afcdb58a
tRDM <85373982+terdim28@users.noreply.github.com>
2023-03-22 14:27:38 +0200

    Update okp4_update.sh

--- cosmos_scripts/okp4/okp4_update.sh
+++ cosmos_scripts/okp4/okp4_update.sh
@@ -6,7 +6,7 @@ git clone https://github.com/okp4/okp4d.git
 cd okp4d || return
 git checkout v4.1.0
 make build
-sudo mv $HOME/go/bin/okp4d /usr/local/bin/okp4d
+sudo mv $HOME/okp4/target/dist/okp4d /usr/local/bin/okp4d
 
 okp4d version
 
